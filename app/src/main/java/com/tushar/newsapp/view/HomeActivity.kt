7bfcd688d8fc5f2d5782b3af94a83ec5d8582f13
Tushar Mahale <tmahale05@gmail.com>
2021-05-14 22:37:41 +0530

    News Project created

---
+++ app/src/main/java/com/tushar/newsapp/view/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.tushar.newsapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tushar.newsapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
