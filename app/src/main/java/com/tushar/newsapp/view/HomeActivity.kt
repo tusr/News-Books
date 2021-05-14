535f7233833d3097a9525a37f24fa0bc1ecc404e
Tushar Mahale <tmahale05@gmail.com>
2021-05-14 23:21:52 +0530

    [HomeActivity] layout updated

--- app/src/main/java/com/tushar/newsapp/view/HomeActivity.kt
+++ app/src/main/java/com/tushar/newsapp/view/HomeActivity.kt
@@ -2,11 +2,15 @@ package com.tushar.newsapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.tushar.newsapp.R
+import com.tushar.newsapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
