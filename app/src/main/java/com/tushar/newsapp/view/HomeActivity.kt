b8c02ab9fe90ba33a0b11fc4530d8fca1795585f
Tushar Mahale <tmahale05@gmail.com>
2021-05-14 23:46:27 +0530

    [NewsFragmentAdapter] NewsFragment Adapter attached

--- app/src/main/java/com/tushar/newsapp/view/HomeActivity.kt
+++ app/src/main/java/com/tushar/newsapp/view/HomeActivity.kt
@@ -1,16 +1,38 @@
 package com.tushar.newsapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.tabs.TabLayoutMediator
 import com.tushar.newsapp.R
 import com.tushar.newsapp.databinding.ActivityHomeBinding
+import com.tushar.newsapp.view.adapter.NewsFragmentAdapter
+
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
+
+    //Tab Texts
+    private val newsTabTexts = arrayOf("News Org", "Google News")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        initViews()
+    }
+
+    //Initializing views
+    private fun initViews() {
+        val newsFragmentAdapter = NewsFragmentAdapter(this)
+        binding.newsPager.adapter = newsFragmentAdapter
+        val tabLayout = binding.tabLayout
+
+        //Attaching tablayout with newsViewPager
+        TabLayoutMediator(tabLayout, binding.newsPager) { tab, position ->
+            //Displaying tab texts 
+            tab.text = newsTabTexts[position]
+            binding.newsPager.setCurrentItem(tab.position, true)
+        }.attach()
     }
 }
