330b88d0a3dea7566a10e427b7dd5a51e0460682
Tushar Mahale <tmahale05@gmail.com>
2021-05-15 20:45:56 +0530

    [AppModule] App module created for DI

--- app/src/main/java/com/tushar/newsapp/di/AppModule.kt
+++ app/src/main/java/com/tushar/newsapp/di/AppModule.kt
@@ -1,19 +1,22 @@
 package com.tushar.newsapp.di
 
-import com.tushar.newsapp.view.GoogleNewsFragment
-import com.tushar.newsapp.view.NewsOrgFragment
+import android.content.Context
+import com.tushar.newsapp.network.NetworkConnectionInterceptor
+
+
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
-    @Provides
-    fun provideNewsOrgFragment(): NewsOrgFragment = NewsOrgFragment()
 
     @Provides
-    fun provideGoogleNewsFragment(): GoogleNewsFragment = GoogleNewsFragment()
+    fun provideNetworkInterceptor(@ApplicationContext context: Context) =
+        NetworkConnectionInterceptor(context)
+
+
 }
